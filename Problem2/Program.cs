bc792b30883debd73a97c7c3093b317ad04ef2bc
nikil <nikil@DESKTOP-73MHGNO>
2021-12-30 17:08:15 -0500

    Add project files.

---
+++ Problem2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int value1 = 1;
+            int value2 = 2;
+            int sum = 2;
+
+            while(value2 < 4000000)
+            {
+                int nextNum = value1 + value2;
+                if (nextNum % 2 == 0)
+                    sum = sum + nextNum;
+                value1 = value2;
+                value2 = nextNum;
+            }
+
+            Console.WriteLine("The sum is: " + sum);
+
+        }
+    }
+}
